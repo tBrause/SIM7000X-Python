954f0a61c9b042ff444c7e8e230bec8534ed99b7
tBrause <brause.torsten@gmail.com>
2025-01-20 00:59:34 +0100

    edit

--- AT.py
+++ AT.py
@@ -35,13 +35,13 @@ def power_down(power_key):
 try:
     power_on(power_key)
     while True:
-        command_input = raw_input('Please input the AT command:')
-        ser.write((command_input+  '\r\n' ).encode())
-        time.sleep(0.1)
-        if ser.inWaiting():
+      command_input = input('Please input the AT command:')
+      ser.write((command_input+  '\r\n' ).encode())
+      time.sleep(0.1)
+      if ser.inWaiting():
             time.sleep(0.01)
             rec_buff = ser.read(ser.inWaiting())
-        if rec_buff != '':
+      if rec_buff != '':
             print(rec_buff.decode())
             rec_buff = ''
 except :
