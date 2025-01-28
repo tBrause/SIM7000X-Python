dd72562106316cdcee931bb2604600a44b7ffbf7
tBrause <brause.torsten@gmail.com>
2025-01-28 21:09:37 +0100

    edit

--- AT-MQTT.py
+++ AT-MQTT.py
@@ -62,8 +62,9 @@ def main():
 
         # 2. Aktiviere MQTT
         print("\n# Aktiviere MQTT")
-        send_at_command(ser, f"AT+CMQTTSTART")
-        send_at_command(ser, f"AT+SMCONN")
+        send_at_command(ser, f"AT+SMCONF=?")
+        #send_at_command(ser, f"AT+CMQTTSTART")
+        #send_at_command(ser, f"AT+SMCONN")
         #send_at_command(ser, f"AT+CMQTTACCQ=0,0,\"{MQTT_SERVER}\"")
         #send_at_command(ser, f"AT+CMQTTCONNECT=0,\"tcp://{MQTT_SERVER}:{MQTT_PORT}\",60,1")
         #send_at_command(ser, f"AT+CMQTTSUB=0,1,\"{MQTT_TOPIC}\",1")
