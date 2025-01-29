0fde4f0aeabe763d40e0b00bf435de173786eba0
tBrause <brause.torsten@gmail.com>
2025-01-29 23:02:25 +0100

    Verbessere AT-PDP.py: Ändere den AT-Befehl zur Deaktivierung des PDP-Profils von 'AT+CGACT=0,13' zu 'AT+CGACT=0,3'

--- AT-PDP.py
+++ AT-PDP.py
@@ -62,7 +62,7 @@ def main():
         print("Status of the profiles (CGACT):", response)
         
         # Deactivate profile 3
-        response = send_at_command(ser, "AT+CGACT=0,13", delay=1)
+        response = send_at_command(ser, "AT+CGACT=0,3", delay=1)
         print("Deactivate profile 3:", response)
         
     except Exception as e:
