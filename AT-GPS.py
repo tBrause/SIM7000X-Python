50c5b5e2be2f981c10cf872a844ab160176bf54c
tBrause <brause.torsten@gmail.com>
2025-01-29 01:15:48 +0100

    Füge AT-GPS.py hinzu: Implementiere AT-Befehle zur Steuerung des GNSS-Moduls

---
+++ AT-GPS.py
@@ -0,0 +1,29 @@
+import serial
+import time
+
+def send_at_command(command, ser):
+    ser.write((command + '\r\n').encode())
+    time.sleep(1)
+    response = ser.read_all().decode()
+    return response
+
+def main():
+    # Open serial port
+    ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+    
+    # Power on GNSS
+    response = send_at_command('AT+CGNSPWR=1', ser)
+    print("Power on GNSS response:", response)
+    
+    # Wait for GNSS to power up
+    time.sleep(2)
+    
+    # Get GNSS information
+    response = send_at_command('AT+CGNSINF', ser)
+    print("GNSS information response:", response)
+    
+    # Close serial port
+    ser.close()
+
+if __name__ == "__main__":
+    main()
